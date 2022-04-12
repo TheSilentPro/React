f763a78341bd474a9b9cfa9d1986310389157473
Silent <perovskiboki@mail.com>
2022-04-12 17:09:25 +0200

    block interact

--- src/main/java/tsp/react/listener/PlayerInteractListener.java
+++ src/main/java/tsp/react/listener/PlayerInteractListener.java
@@ -1,6 +1,7 @@
 package tsp.react.listener;
 
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -8,8 +9,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import tsp.react.React;
+import tsp.react.reactable.ReactBlock;
 import tsp.react.reactable.ReactItem;
 import tsp.react.reactable.Reactable;
+import tsp.react.util.ReactBlockData;
 
 import java.util.Optional;
 
@@ -23,8 +26,9 @@ public class PlayerInteractListener implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        // ReactItem
         ItemStack item = event.getItem();
-        if (item == null || !item.hasItemMeta()) {
+        if (item == null || item.getItemMeta() == null) {
             return;
         }
         ItemMeta meta = item.getItemMeta();
@@ -41,6 +45,32 @@ public class PlayerInteractListener implements Listener {
 
         if (reactable instanceof ReactItem) {
             ((ReactItem) reactable).onInteract(event);
+        }
+
+        // ReactBlock
+        Block block = event.getClickedBlock();
+        if (block == null) {
+            return;
+        }
+
+        ReactBlockData data = new ReactBlockData(block, react.getPlugin());
+        if (data.isEmpty()) {
+            return;
+        }
+
+        String blockId = data.get(react.getManager().getReactId(), PersistentDataType.STRING);
+        if (blockId == null) {
+            return;
+        }
+
+        Optional<Reactable> optionalBlock = react.getManager().get(NamespacedKey.fromString(blockId));
+        if (!optionalBlock.isPresent()) {
+            return;
+        }
+        Reactable reactableBlock = optional.get();
+
+        if (reactableBlock instanceof ReactBlock) {
+            ((ReactBlock) reactable).onInteract(event);
         }
     }
 
